de0b7b829cd674e6c7b924f32d5b09d7477ada68
Sven Hodapp <sven.hodapp@gmail.com>
2015-05-24 17:57:26 +0300

    Read from XML to Graph-Database

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -7,34 +7,30 @@ import play.api.Play.current
 
 import scala.xml.XML
 
-import com.tinkerpop.blueprints.impls.orient.OrientGraph
+import com.tinkerpop.blueprints.impls.orient.OrientGraphFactory
 
 import scala.collection.JavaConverters._
 import scala.collection.JavaConversions._
 
 object Application extends Controller {
+  val factory = new OrientGraphFactory("plocal:/tmp/modd-devl").setupPool(1,10)
+  def graph = factory.getTx
+
   def index = Action {
-    eu.themerius.util.DocElemXmlReader.readXml
-    Graph.init
-    val query = Graph.graph.query.has("type", "Test").vertices.toList
-    Ok(views.html.main("Index")(s"Your new application is ready. ${query.map(_.getProperty("model").asInstanceOf[String])}"))
+    eu.themerius.util.DocElemXmlReader.toDatabase
+    //Graph.init
+    //val query = Graph.graph.query.has("type", "Paragraph").vertices.toList
+    //Ok(views.html.main("Index")(s"Your new application is ready. ${query.map(_.getProperty("model").asInstanceOf[String])}"))
+    Ok(views.html.main("Index")(s"Your new application is ready."))
+
   }
 
-}
-
-object Graph {
-  val graph = new OrientGraph("plocal:/tmp/modd-devl")
-  var ifinit = false
-
-  def init = if (!ifinit) {
-    val docelem = graph.addVertex("class:DocElem", "type", "TEST")
-    docelem.setProperty("type", "Test")
-    docelem.setProperty("model", "Serialized Model!")
-    docelem.setProperty("uri", "aaaa")
-    graph.commit()
-    docelem
-    ifinit = true
+  def docElem(uuid: String) = Action {
+    val res = graph.query.has("uuid", uuid).vertices.toList
+    val model = res.map(_.getProperty("model").asInstanceOf[String])
+    Ok(views.html.main(s"DocElem ${uuid}")(model(0)))
   }
+
 }
 
 // http://www.scala-js.org
--- app/eu/themerius/DocElemXmlReader.scala
+++ app/eu/themerius/DocElemXmlReader.scala
@@ -4,14 +4,43 @@ import scala.xml.XML
 import play.api.Play
 import play.api.Play.current
 
+import com.tinkerpop.blueprints.impls.orient.OrientGraphFactory
+
+import scala.collection.JavaConverters._
+import scala.collection.JavaConversions._
+
+case class DocElem(id: String, dtype: String, model: String)
+// mtype für modell typ
+
 object DocElemXmlReader {
+  val factory = new OrientGraphFactory("plocal:/tmp/modd-devl").setupPool(1,10)
+  def getGraph = factory.getTx
+
   def readXml = {
     Play.resourceAsStream("public/docs/About.xml") match {
       case Some(file) => {
         val xml = XML.load(file)
-        println(xml \\ "modd" \\ "docelems" \\ "docelem" \ "model" map (_.text))
+        val docElems = xml \\ "modd" \\ "docelems" \\ "docelem"
+        docElems.map(
+          d => DocElem(d \ "id" text, d \ "type" text, d \ "model" text)
+        )
       }
-      case None => println("Not found")
+      case None => Nil
     }
+  }
+
+  def toDatabase = {
+    val docElems = readXml
+    println(docElems)
+    val g = getGraph
+    docElems.map { d =>
+      val v = g.addVertex(
+        "class:DocElem",
+        "type", d.dtype,
+        "model", d.model,
+        "uuid", d.id
+      )
+    }
+    g.commit()
   }
 }
